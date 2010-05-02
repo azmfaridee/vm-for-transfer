b151445cb7fec54583f5cb09d2441de828f54a35
darthxaher <darthxaher@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-05-02 14:43:56 +0000

    New file
    git-svn-id: http://apertium.svn.sourceforge.net/svnroot/apertium/branches/gsoc2010/darthxaher/vm-for-transfer@22459 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ dev/transferword.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+class TransferWord(object):
+    def __init__(self, slword, tlword):
+        self.slword = slword
+        self.tlword = tlword
+
+    def __str__(self):
+        return "SL: " + self.slword + ", TL: " + self.tlword
+
+class TransferWordFactory(object):
+    """
+    Not a factory in a true sense
+    """
+
+    def __init__(self, string):
+        self.string = string
+        self.transferwords = []
+
+    def generate(self):
+        """
+        FIXME: Very naive right now, discards blanks, will fix that later
+        """
+        while self.string:
+            part = self.string[self.string.index('^')+1:self.string.index('$')]
+            self.string = self.string[self.string.index('$')+1:]
+            self.transferwords.append(TransferWord(*part.split('/')))
+            
+    def getTransferWord(self):
+        return self.transferwords
+ 
+# echo "I eat rice" | apertium -d . en-es-tagger
+# ^prpers<prn><subj><p1><mf><sg>$ ^eat<vblex><pres>$ ^rice<n><sg>$^.<sent>$
+# echo "I eat rice" | apertium -d . en-es-tagger | lt-proc -b en-es.autobil.bin
+# ^prpers<prn><subj><p1><mf><sg>/prpers<prn><tn><p1><mf><sg>$ ^eat<vblex><pres>/comer<vblex><pres>$ ^rice<n><sg>/arroz<n><m><sg>$^.<sent>/.<sent>$
+
+if __name__ == "__main__":
+    string = "^prpers<prn><subj><p1><mf><sg>/prpers<prn><tn><p1><mf><sg>$ ^eat<vblex><pres>/comer<vblex><pres>$ ^rice<n><sg>/arroz<n><m><sg>$^.<sent>/.<sent>$"
+    transferWordFactory = TransferWordFactory(string)
+    transferWordFactory.generate()
+    words = transferWordFactory.getTransferWord()
