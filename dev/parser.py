f2ca6dab8ead28ef2bc4d3afe7a907cc3c0fb7bb
darthxaher <darthxaher@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-07-21 17:28:52 +0000

    creating package
    git-svn-id: http://apertium.svn.sourceforge.net/svnroot/apertium/branches/gsoc2010/darthxaher/vm-for-transfer@24092 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/parser.py
+++ dev/parser.py
@@ -3,15 +3,15 @@
 # -*- encoding: utf-8 -*-
 
 from pprint import pprint
-from expatparser import ExpatParser
-from parentrecord import ParentRecord
-from callstack import CallStack
+from compiler.expatparser import ExpatParser
+from compiler.parentrecord import ParentRecord
+from compiler.callstack import CallStack
+from compiler.symboltable import SymbolTable
 
 # clip, lit-tag need special handling if inside of any of these tags
 delayed_tags = ['let', 'modify-case']
 
 DEBUG_MODE = False
- 
         
 class EventHandler(object):
     def __init__(self, compiler):
@@ -403,51 +403,6 @@ class EventHandler(object):
     def handle_concat_end(self, event, codebuffer):
         pass
     
-class SymbolTable(object):
-    def __init__(self, callStack):
-        self.symbolList = {}
-        self.childList = {}
-
-        self.currentSymbolId = 0
-
-        self.callStack = callStack
-
-    def addSymbol(self, event):
-        # symbol id starts from 1
-        self.currentSymbolId += 1        
-        self.symbolList[self.currentSymbolId] = event
-
-        currentParentId = -1
-        try: currentParent = self.callStack.getTop(2)
-        except: currentParent = None
-        currentParentId = self.__getId(currentParent)
-
-        if currentParentId not in self.childList.keys():
-            self.childList[currentParentId] = []
-        self.childList[currentParentId].append(self.currentSymbolId)
-
-    def __getId(self, event):
-        eventId = -1
-        # this is only for safety, most of the time eventId would be
-        # equal to currentSymbolId
-        for i in range(self.currentSymbolId, 0, -1):
-            if self.symbolList[i] == event:
-                return i
-        return eventId
-            
-
-    def getChilds(self, event):
-        eventId = self.__getId(event)
-        ## print event
-        ## print eventId
-        ## pprint(self.childList)
-        ## pprint(self.symbolList)
-        ## print self.symbolList[719]
-        childs = []
-        for childId in self.childList[eventId]:
-            childs.append(self.symbolList[childId])
-        return childs
-        
 
 class Compiler(object):
     """
